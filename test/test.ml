26023e57411f4c7e31b581c622c0ef3ddab852f1
Mika Illouz <mika@illouz.net>
2020-06-23 23:55:52 -0700

    For testing, using handrolled Gaussian RNG, which makes library independent of package `gsl` .

--- test/test.ml
+++ test/test.ml
@@ -1,40 +1,63 @@
 (* In this test, we measure the error between the true quantiles of
    data, and the quantiles as computed through the [Bentov] histogram
    approximation module. The data is drawn from a mixture of two
-   Gaussians, N(2,1) and N(5,1), where the mixing coefficient is
-   1/2. We compute the approximate quantiles in two ways: In the
-   frist, we simply add each sample into a [Bentov.histogram]. We call
-   this histogram [mixed].  In the second, we add a datum to one of
-   two [Bentov.histogram]'s, one associated with each of the
-   Guassians..  We then merge these two histograms using
-   [Bentov.merge].  We call the result of merging the two
-   sub-histograms [merged].  Finally, we compute and print the
-   mean-square-error between the true quantiles and [mixed], and the
-   true quantiles and [merged]. *)
-
-open Gsl.Randist
-
-(* draw a sample from N([mu],[sigma]^2) *)
-let normal rng ~mu ~sigma =
-  mu +. (gaussian rng ~sigma)
+   Gaussians, N(2,1) and N(5,1), where the mixing coefficient is 1/2.
+   We compute the approximate quantiles in two ways: In the frist, we
+   simply add each sample into a [Bentov.histogram]. We call this
+   histogram [mixed]. In the second, we add a datum to one of two
+   [Bentov.histogram]'s, one associated with each of the Guassians. We
+   then merge these two histograms using [Bentov.merge]. We call the
+   result of merging the two sub-histograms [merged]. Finally, we
+   compute and print the mean-square-error between the true quantiles
+   and [mixed], and the true quantiles and [merged]. *)
 
 (* [quantiles list num_intervals] returns the quantiles (true, not
    approximated) of list [list] at [num_intervals + 1] points, including
    the minimum and maximum values (which are the first and last values of
    the result, resp. *)
-let quantiles list num_intervals =
-  let num_intervals_f = float num_intervals in
-  let array = Array.of_list list in
-  Array.sort Stdlib.compare array;
-  let rec loop i accu =
-    if i > num_intervals then
-      List.rev accu
-    else
-      let q = (float i) /. num_intervals_f in
-      let x = Gsl.Stats.quantile_from_sorted_data array q in
-      loop (i+1) ((i,x) :: accu)
+let quantiles =
+  let rec loop i j span accu = function
+    | x0 :: x1 :: rest ->
+      let s = (float j) *. span in
+      if i <= s && s < i +. 1. then
+        let d = s -. i in
+        let x_d = (x1 -. x0) *. d +. x0 in
+        loop i (j + 1) span ((j, x_d) :: accu) (x0 :: x1 :: rest)
+      else
+        loop (i +. 1.) j span accu (x1 :: rest)
+
+    | [x] ->
+      let x_d = j, x in
+      List.rev (x_d :: accu)
+
+    | [] -> assert false
   in
-  loop 0 []
+  fun x m ->
+    let x_sorted = List.sort Stdlib.compare x in
+    let n = List.length x_sorted in
+    let span = (float n) /. (float m) in
+    loop 0. 0 span [] x_sorted
+
+
+open Seq
+
+(** [up_to n seq] returns a sequence that echos at most [n] values of
+    sequence [seq] *)
+let up_to =
+  let rec loop i n seq () =
+    if i = n then
+      Nil
+    else
+      match seq () with
+      | Nil -> Nil
+      | Cons (v, seq) ->
+        Cons (v, loop (i + 1) n seq)
+  in
+  fun n seq ->
+    if n < 0 then
+      raise (Invalid_argument "up_to")
+    else
+      loop 0 n seq
 
 module IntMap = Map.Make(Int)
 
@@ -51,36 +74,47 @@ let _ =
   (* the size of the approximate histograms *)
   let q = int_of_string Sys.argv.(2) in
 
-  let rng = Gsl.Rng.(make (default ())) in
-
-  (* draw [x] from one of two Gaussians, adding it to the [mixed]
-     approximate histogram, and one of [n1_h] and [n2_h] approximate
-     histograms *)
-  let rec loop i n1_h n2_h mixed_h data =
-    if i < n then
-      if Gsl.Rng.uniform rng > 0.5 then
-        let x = normal rng ~mu:2.0 ~sigma:1.0 in (* N(2,1) *)
-        let n1_h = Bentov.add x n1_h in
-        let mixed_h = Bentov.add x mixed_h in
-        loop (i+1) n1_h n2_h mixed_h (x :: data)
-      else
-        let x = normal rng ~mu:5.0 ~sigma:1.0 in (* N(5,1) *)
-        let n2_h = Bentov.add x n2_h in
-        let mixed_h = Bentov.add x mixed_h in
-        loop (i+1) n1_h n2_h mixed_h (x :: data)
+  let rec gaussian_mixture normal_a normal_b () =
+    if Random.bool () then
+      match normal_a () with
+      | Cons (x, normal_a) ->
+        Cons ((`A, x), gaussian_mixture normal_a normal_b)
+      | Nil -> Nil
     else
-      n1_h, n2_h, mixed_h, data
+      match normal_b () with
+      | Cons (x, normal_b) ->
+        Cons ((`B, x), gaussian_mixture normal_a normal_b)
+      | Nil -> Nil
   in
+
+  let n1 = Gaussian.seq ~mu:2.0 ~sigma:1.0 in (* N(2,1) *)
+  let n2 = Gaussian.seq ~mu:5.0 ~sigma:1.0 in (* N(5,1) *)
+
+  let gmm = up_to n (gaussian_mixture n1 n2) in
 
   let open Bentov in
 
-  let n1_h = create q in
-  let n2_h = create q in
-  let mixed_h = create q in
-  let n1_h, n2_h, mixed_h, data = loop 0 n1_h n2_h mixed_h [] in
+  let normal_a_h, normal_b_h, mixed_h, data = Seq.fold_left (
+    fun (normal_a_h, normal_b_h, mixed_h, data) draw ->
+      match draw with
+      | `A, x ->
+        let normal_a_h = Bentov.add x normal_a_h in
+        let mixed_h = Bentov.add x mixed_h in
+        normal_a_h, normal_b_h, mixed_h, x :: data
+
+      | `B, x ->
+        let normal_b_h = Bentov.add x normal_b_h in
+        let mixed_h = Bentov.add x mixed_h in
+        normal_a_h, normal_b_h, mixed_h, x :: data
+  ) (
+    let normal_a_h = create q in
+    let normal_b_h = create q in
+    let mixed_h = create q in
+    normal_a_h, normal_b_h, mixed_h, []
+  ) gmm in
 
   (* merge the two sub-histograms *)
-  let merged_h = merge [n1_h; n2_h] q in
+  let merged_h = merge [normal_a_h; normal_b_h] q in
 
   assert (total_count mixed_h = n );
   assert (total_count merged_h = n );
